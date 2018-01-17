a32f441c111688f8bb3678031ea460f59bf58250
atarek <atarek@EGY-ATAREK-NB3>
2018-01-17 16:18:47 +0200

    Project Initialization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCutqia8tVtvuKDJRt6EWy421Eau-_raPE",
+    authDomain: "oshop-b2593.firebaseapp.com",
+    databaseURL: "https://oshop-b2593.firebaseio.com",
+    projectId: "oshop-b2593",
+    storageBucket: "",
+    messagingSenderId: "395158741647"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCutqia8tVtvuKDJRt6EWy421Eau-_raPE",
+    authDomain: "oshop-b2593.firebaseapp.com",
+    databaseURL: "https://oshop-b2593.firebaseio.com",
+    projectId: "oshop-b2593",
+    storageBucket: "",
+    messagingSenderId: "395158741647"
+  }
 };
